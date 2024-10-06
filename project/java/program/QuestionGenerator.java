15cd6a61909f67343ea22ff3f0e4b98762b2e246
Samuel46446 <156199948+Samuel46446@users.noreply.github.com>
2024-10-06 22:40:58 +0200

    testing generation

--- project/java/program/QuestionGenerator.java
+++ project/java/program/QuestionGenerator.java
@@ -13,25 +13,75 @@ public final class QuestionGenerator
     private QuestionGenerator() {}
     //private static final char LINE_NEW = '\n';
     
-    public static void main(String[] args)
+    private static void lvl2()
     {
-        String bonneReponseTexte = "Bonne réponse";
+        List<String> questions = new ArrayList<>();
+        List<String> languages = new ArrayList<>();
+        List<String> codes = new ArrayList<>();
 
-        // Mauvaises réponses
-        String mauvaiseReponseTexte1 = "Mauvaise réponse 1";
-        String mauvaiseReponseTexte2 = "Mauvaise réponse 2";
-        String mauvaiseReponseTexte3 = "Mauvaise réponse 3";
+        questions.add("Quel est l'erreur dans ce programme C");
+        languages.add("c");
+        codes.add("int main (void)\r\n" + //
+                        "{\r\n" + //
+                        "    int i = 4 / 0;\r\n" + //
+                        "    printf(\"Div : %d\", i);\r\n" + //
+                        "    return 0;\r\n" + //
+                        "}");
+        List<String> reponses1 = List.of(
+        formatButton("Division par 0 impossible", true), // True pour la bonne réponse (button)
+        formatButton("Méthode main non correcte", false), // False pour mauvaise réponse (link)
+        formatButton("Changement de type de int à double", false),
+        formatButton("Impression console incorrecte", false));
 
-                List<String> reponses = new ArrayList<>();
-        reponses.add(formatButton(bonneReponseTexte, true)); // True pour la bonne réponse (button)
-        reponses.add(formatButton(mauvaiseReponseTexte1, false)); // False pour mauvaise réponse (link)
-        reponses.add(formatButton(mauvaiseReponseTexte2, false));
-        reponses.add(formatButton(mauvaiseReponseTexte3, false));
+        questions.add("Quel type de variable régit les conditions suivantes");
+        languages.add("java");
+        codes.add("void a()\r\n" + //
+                        "    {\r\n" + //
+                        "        int a = 45;\r\n" + //
+                        "        int b = 46;\r\n" + //
+                        "\r\n" + //
+                        "        if(b > a)\r\n" + //
+                        "        {\r\n" + //
+                        "            System.out.println(\"B est plus grand\");\r\n" + //
+                        "        }\r\n" + //
+                        "        else\r\n" + //
+                        "        {\r\n" + //
+                        "            System.out.println(\"A est plus grand\");\r\n" + //
+                        "        }\r\n" + //
+                        "    }");
+        List<String> reponses2 = List.of(
+            formatButton("Boolean", true), // True pour la bonne réponse (button)
+            formatButton("Integer", false), // False pour mauvaise réponse (link)
+            formatButton("Double", false),
+            formatButton("Condition", false));
 
-        genQuestion(2, List.of("","","",""), List.of("","","",""), List.of("","","",""), reponses);
+        questions.add("Que produit ce programme sql");
+        languages.add("sql");
+        codes.add("SELECT * FROM table1;");
+        List<String> reponses3 = List.of(
+                formatButton("Affiche toute les lignes et colonnes de la table1", true), // True pour la bonne réponse (button)
+                formatButton("Affiche toute les tables de la base de données", false), // False pour mauvaise réponse (link)
+                formatButton("Créer une table", false),
+                formatButton("Modifie une table", false));
+        
+        questions.add("Que font les balise h en html");
+        languages.add("html");
+        codes.add("<h1>Titre de l'onglet</h1>");
+        List<String> reponses4 = List.of(
+                formatButton("Affiche un texte sur la page html", true), // True pour la bonne réponse (button)
+                formatButton("Affiche un titre à l'onglet html", false), // False pour mauvaise réponse (link)
+                formatButton("Affiche une boîte de dialogue sur la page html", false),
+                formatButton("Change l'url d'une page html", false));
+
+        genQuestion(2, questions, languages, codes, reponses1, reponses2, reponses3, reponses4);
     }
 
-    private static void genQuestion(int lvl, List<String> problems, List<String> setLanguages, List<String> codes, List<String> reponsesListed)
+    public static void main(String[] args)
+    {
+        lvl2();
+    }
+
+    private static void genQuestion(int lvl, List<String> problems, List<String> setLanguages, List<String> codes, List<String> reponsesListed1, List<String> reponsesListed2, List<String> reponsesListed3, List<String> reponsesListed4)
     {
         File directory = new File(System.getProperty("user.dir")+File.separator+"quests"+File.separator+lvl+File.separator);
 
@@ -45,10 +95,10 @@ public final class QuestionGenerator
         File quest3 = new File(directory.getAbsolutePath()+File.separator+"quest3.html");
         File quest4 = new File(directory.getAbsolutePath()+File.separator+"quest4.html");
 
-        writeQuestion(quest1, lvl, problems.get(0), setLanguages.get(0), codes.get(0), reponsesListed);
-        writeQuestion(quest2, lvl, problems.get(1), setLanguages.get(1), codes.get(1), reponsesListed);
-        writeQuestion(quest3, lvl, problems.get(2), setLanguages.get(2), codes.get(2), reponsesListed);
-        writeQuestion(quest4, lvl, problems.get(3), setLanguages.get(3), codes.get(3), reponsesListed);
+        writeQuestion(quest1, lvl, problems.get(0), setLanguages.get(0), codes.get(0), reponsesListed1);
+        writeQuestion(quest2, lvl, problems.get(1), setLanguages.get(1), codes.get(1), reponsesListed2);
+        writeQuestion(quest3, lvl, problems.get(2), setLanguages.get(2), codes.get(2), reponsesListed3);
+        writeQuestion(quest4, lvl, problems.get(3), setLanguages.get(3), codes.get(3), reponsesListed4);
     }
 
     private static void writeQuestion(File questIn, int lvl, String problem, String setLanguage, String code, List<String> reponses)
@@ -57,18 +107,19 @@ public final class QuestionGenerator
 
         if(!questIn.exists())
         {
-			try(FileWriter writer = new FileWriter(questIn))
+			try
 			{
+                FileWriter writer = new FileWriter(questIn);
 				BufferedWriter buffer = new BufferedWriter(writer);				
 				
 				buffer.write("<!DOCTYPE html>\r\n" + //
                                         "<html lang=\"fr\">\r\n" + //
                                         "    <head>\r\n" + //
                                         "      <title> Question "+lvl+" </title>\r\n" + //
-                                        "      <link rel=\"icon\" href=\"textures/logo.png\" type=\"image/png\">\r\n" + //
+                                        "      <link rel=\"icon\" href=\"../textures/logo.png\" type=\"image/png\">\r\n" + //
                                         "      <meta charset=\"utf-8\">\r\n" + //
                                         "      <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n" + //
-                                        "      <link rel=\"stylesheet\" type=\"text/css\" href=\"css/index.css\">\r\n" + //
+                                        "      <link rel=\"stylesheet\" type=\"text/css\" href=\"../css/index.css\">\r\n" + //
                                         "\r\n" + //
                                         "          <!-- Inclusion de Highlight.js -->\r\n" + //
                                         "    <link href=\"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/default.min.css\" rel=\"stylesheet\">\r\n" + //
@@ -107,7 +158,7 @@ public final class QuestionGenerator
                                         "        </table>\r\n" + //
                                         "        \r\n" + //
                                         "        <script>\r\n" + //
-                                        "            const pages = [\"2/quest1.html\", \"2/quest2.html\", \"2/quest3.html\", \"2/quest4.html\"];\r\n" + //
+                                        "            const pages = [\""+(lvl+1)+"/quest1.html\", \""+(lvl+1)+"/quest2.html\", \""+(lvl+1)+"/quest3.html\", \""+(lvl+1)+"/quest4.html\"];\r\n" + //
                                         "            document.getElementById('randomPageButton').onclick = function() {\r\n" + //
                                         "                const randomPage = pages[Math.floor(Math.random() * pages.length)];\r\n" + //
                                         "                window.location.href = randomPage;\r\n" + //
