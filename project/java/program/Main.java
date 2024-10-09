51f22b70dcf3d404a33adf7da3d082c619cb8639
Samuel46446 <156199948+Samuel46446@users.noreply.github.com>
2024-10-09 21:38:56 +0200

    commit

--- project/java/program/Main.java
+++ project/java/program/Main.java
@@ -4,19 +4,3 @@ public class Main
 {
     
 }
-
-void a()
-{
-    int a = 45;
-    int b = 46;
-
-    if(b > a)
-    {
-        System.out.println("B est plus grand");
-    }
-    else
-    {
-        System.out.println("A est plus grand");
-    }
-}
-
