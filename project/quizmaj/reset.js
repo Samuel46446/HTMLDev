70b53538c981d527e288712756c4420cd7d435fc
Samuel46446 <156199948+Samuel46446@users.noreply.github.com>
2024-11-07 00:38:37 +0100

    update and finish java quiz

--- project/quizmaj/reset.js
+++ project/quizmaj/reset.js
@@ -1,6 +1,10 @@
 function resetTotal()
 {
     localStorage.setItem('javaPoint', 0);
+    localStorage.setItem('cppPoint', 0);
+    localStorage.setItem('pythonPoint', 0);
+    localStorage.setItem('webPoint', 0);
+    localStorage.setItem('sqlPoint', 0);
 }
 
 resetTotal();
